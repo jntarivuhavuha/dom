1484c5c2abd0eaf9e25253abdd8cb992a1a7ba2d
jntarivuhavuha <114191852+jntarivuhavuha@users.noreply.github.com>
2023-02-03 16:51:55 +1100

    Add files via upload

---
+++ BOM/js/script.js
@@ -0,0 +1,24 @@
+﻿let list = document.querySelector('ul');
+let input = document.querySelector('input');
+let button = document.querySelector('button');
+
+button.onclick = () => {
+  let myItem = input.value;
+  input.value = '';
+  let listItem = document.createElement('li');
+  let listText = document.createElement('span');
+  let listBtn = document.createElement('button');
+  
+  listItem.appendChild(listText);
+  listText.textContent = myItem;
+  listItem.appendChild(listBtn);
+  listBtn.textContent = 'X';
+  
+  list.appendChild(listItem);  
+  
+  listBtn.onclick = (e) => {
+    list.removeChild(listItem);
+  }
+
+input.focus();
+}
